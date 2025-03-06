4ce1030fd564c9929544268429c7dfb4d18e808a
gaddisai <saikrishna.gaddii@gmail.com>
2025-03-06 11:37:21 +0530

    chages dones

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+//we can use <div></div> or <></> to wrap the content
 import './App.css';
 
-function App() {
+function App(value) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello {value.name}! My age is {value.age}</h1>
+    </>
   );
 }
 
-export default App;
+export{App};
