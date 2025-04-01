b7a4e98912b5396f01bf99a4a1dba76af418168c
gaddisai <saikrishna.gaddii@gmail.com>
2025-04-01 12:28:10 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -7,22 +7,31 @@ import { Footer } from './components/Footer';
 import { Register } from './components/Register';
 import { BrowserRouter,Route,Routes } from 'react-router-dom'; 
 import { createContext,useState } from "react";
-export const appContext = createContext();
-
+const appContext = createContext();
+export {appContext}
 function App() {
+  const products = [
+    { id: 1, name: 'Product 1', price: 100 },
+    { id: 2, name: 'Product 2', price: 200 },
+    { id: 3, name: 'Product 3', price: 300 },
+    { id: 4, name: 'Product 4', price: 400 },
+    { id: 5, name: 'Product 5', price: 400 },
+    { id: 6, name: 'Product 6', price: 400 },
+  ];
   const [users, setUsers] = useState([]);
   const [user, setUser] = useState({});
+  const [cart,setCart] = useState([]);
+
   return (
     <div>
       <BrowserRouter>
-      <appContext.Provider value={{ users, setUsers, user, setUser }}>
+      <appContext.Provider value={{ users, setUsers, user, setUser, products , cart , setCart }}>
         <Header/>
         <Routes>
           <Route index element={<Products/>}/>
-          <Route path="/products" element={<Products/>}/>
-          <Route path="/login" element={<Login/>}/>  
+          <Route path="/products" element={<Products/>}/> 
           <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />}></Route>
+          <Route path="/login" element={<Login />}/>
         </Routes>
         <Footer/>
       </appContext.Provider>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,18 +1,8 @@
 import './Products.css';
 import { useContext } from 'react';
 import { appContext } from '../App';
-
 function Products() {
-    const { user }= useContext(appContext);
-    const products = [
-        { id: 1, name: 'Product 1', price: 100 },
-        { id: 2, name: 'Product 2', price: 200 },
-        { id: 3, name: 'Product 3', price: 300 },
-        { id: 4, name: 'Product 4', price: 400 },
-        { id: 5, name: 'Product 5', price: 400 },
-        { id: 6, name: 'Product 6', price: 400 },
-    ];
-
+    const { user,products }= useContext(appContext);
     return (
         <div className="App_Products_Container">
             <h2>Welcome {user.name}</h2>
